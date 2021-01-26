1fcd3094ad529ed2f735f7e4044e6a3f5f5d45a8
songdalin <songdalin@qdcares.cn>
2021-01-26 18:45:39 +0800

    添加redis scan

---
+++ start/src/main/java/com/example/thread/ProductModel.java
@@ -0,0 +1,20 @@
+package com.example.thread;
+
+/**
+ * @author ：songdalin
+ * @date ：2021-01-26 下午 06:42
+ * @description：模型
+ * @modified By：
+ * @version: 1.0
+ */
+public class ProductModel {
+
+    /**
+     *
+     * 多线程测试学习
+     * volatile
+     * synchronized
+     */
+
+    int
+}
